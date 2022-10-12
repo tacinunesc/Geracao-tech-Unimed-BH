76083432c0d90b2567c8ca2ff8ff6013a00b120c
Tacila Nunes <65735519+tacinunesc@users.noreply.github.com>
2022-10-12 20:03:25 -0300

    Delete Desktop/Java/cartApi directory

--- Desktop/Java/cartApi/src/main/java/com/ame/cartApi/repository/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ame.cartApi.repository;
-
-import com.ame.cartApi.model.Card;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardRepository extends JpaRepository<Card, Long> {}
